53ff55ccf3964eec4532adf79254322122fcf59a
Kayne <kayne@kayne.com>
2018-03-12 23:59:53 -0500

    Finalized project

--- Project1Java.js
+++ Project1Java.js
@@ -3,9 +3,6 @@ $(function(){
     $(".black1").hide().show(function(){
       $(".black1").delay(400).removeClass("black1").addClass("red").delay(200).show(200).text("T");
     });
-    $("#progressbar").delay(600).show(function(){
-      $(this)
-    });
     $(".black2").hide().delay(600).show(200);
     $(".black3").hide().delay(800).show(200);
     $(".black4").hide().delay(1000).show(200);
@@ -16,11 +13,11 @@ $(function(){
       $(this).delay(1200).prop("checked", true);
     });
     $("#volumeOff").hide().delay(2200).show(function(){
-      $(this).delay(100).toggleClass("glyphicon glyphicon-volume-off").show(function(){
-        $(this).toggleClass("glyphicon glyphicon-volume-up")
+      $(this).delay().toggleClass("glyphicon glyphicon-volume-off").show(function(){
+        $(this).toggleClass("glyphicon glyphicon-volume-up").css("font-size", "30px")
       });
     });
-    $("#alert").delay(3000).show(function(){
+    $("#alert").hide().delay(2400).show(function(){
       $(this).toggleClass("alert alert-danger").show(function(){
         $(this).toggleClass("alert alert-success").text("Yay! Your volume is now on. Click finish to reset.")
       })
